024eec98102579d92ea46db7b827addd115777c2
ihortok <itokaryk@gmail.com>
2022-04-17 17:47:39 +0300

    refactor refactored method

--- lib/frequency_counter_pattern/same_array.rb
+++ lib/frequency_counter_pattern/same_array.rb
@@ -19,17 +19,13 @@ end
 def same_array_refactored(arr1, arr2)
   return false unless arr1.size == arr2.size
 
-  hash1 = {}
   hash2 = {}
-
-  arr1.each { |n| hash1.has_key?(n) ? hash1[n] += 1 : hash1[n] = 1 }
 
   arr2.each { |n| hash2.has_key?(n) ? hash2[n] += 1 : hash2[n] = 1 }
 
-  hash1_keys = hash1.keys
   hash2_keys = hash2.keys
 
-  hash1_keys.each { |k| return false unless hash2_keys.include? k**2 }
+  arr1.each { |n| return false unless hash2_keys.include? n**2 }
 
   true
 end
